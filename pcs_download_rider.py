4f8a25dc79cc2272fb8c94d9c477f4e253dded01
Klemen Ziberna <ziberna@gmail.com>
2017-04-30 13:13:19 +0100

    Rider weight

--- pcs_download_rider.py
+++ pcs_download_rider.py
@@ -35,22 +35,32 @@ rider_main_page_soup = bs(rider_main_page.content, 'html.parser')
 
 rider_overview_DOB
 rider_overview_Nationality
-rider_overview_Weight
+
+# Rider weight
+Weight_find = rider_main_page_soup.find_all(string=re.compile('Weight'))
+rider_overview_Weight = \
+    str(Weight_find[0].parent.next_sibling.string)
+rider_overview_Weight = float(rider_overview_Weight.split('kg', 1)[0])
+
+
 rider_overview_Height
+
+
+
 rider_overview_Team2017
 
 # Points by speciality - One day races
 OneDayRaces_find = rider_main_page_soup.find_all(string=re.compile('One day races'))
-rider_overview_OneDayRaces = str(OneDayRaces_find[0].parent.previous_sibling.string)
+rider_overview_OneDayRaces = int(OneDayRaces_find[0].parent.previous_sibling.string)
 
 # Points by speciality - GC
 GC_find = rider_main_page_soup.find_all(string=re.compile('GC'))
-rider_overview_GC = str(GC_find[0].parent.previous_sibling.string)
+rider_overview_GC = int(GC_find[0].parent.previous_sibling.string)
 
 # Points by speciality - Time trial
 TimeTrial_find = rider_main_page_soup.find_all(string=re.compile('Time trial'))
-rider_overview_TimeTrial = str(TimeTrial_find[0].parent.previous_sibling.string)
+rider_overview_TimeTrial = int(TimeTrial_find[0].parent.previous_sibling.string)
 
 # Points by speciality - Sprint
 Sprint_find = rider_main_page_soup.find_all(string=re.compile('Sprint'))
-rider_overview_Sprint = str(Sprint_find[0].parent.previous_sibling.string)
+rider_overview_Sprint = int(Sprint_find[0].parent.previous_sibling.string)
